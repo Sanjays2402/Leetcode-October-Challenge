0b5411f9e9e69861cdc4d97aba6d082eba3544ee
Sanjay <sanjays2402@gmail.com>
2020-10-10 21:26:33 +0530

    Create October 10 Solution.cpp

---
+++ October 10/October 10 Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b)
+    {
+        return a[1] < b[1];
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        
+        if(points.size() == 0)
+            return 0;
+        
+        if(points.size() == 1)
+            return 1;
+        
+        int count=1;
+        sort(points.begin(), points.end(), comp);
+        
+        int temp=points[0][1];
+        for(int i=1; i<points.size(); i++)
+        {
+            if(temp < points[i][0])
+            {
+                count++;
+                temp = points[i][1];
+            }
+        }
+        return count;
+    }
+};
